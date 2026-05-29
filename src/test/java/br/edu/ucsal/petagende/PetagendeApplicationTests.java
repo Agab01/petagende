85e286eb5e34b3939053ca8b6262501f75fd8020
Agab <agab06@yahoo.com.br>
2026-05-29 16:58:09 -0300

    chore: Arquivos auxiliares do Maven, testes e configuracoes do Git

---
+++ src/test/java/br/edu/ucsal/petagende/PetagendeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ucsal.petagende;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetagendeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

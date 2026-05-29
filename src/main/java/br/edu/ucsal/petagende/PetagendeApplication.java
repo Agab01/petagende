dcc9bc11d01addbe379cae9c9bd1c216decc1164
Agab <agab06@yahoo.com.br>
2026-05-29 16:54:07 -0300

    Setup: Inicializacao do projeto Spring Boot e conexao PostgreSQL

---
+++ src/main/java/br/edu/ucsal/petagende/PetagendeApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ucsal.petagende;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetagendeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetagendeApplication.class, args);
+	}
+
+}
